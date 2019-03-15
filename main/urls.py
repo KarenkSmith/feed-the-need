d88a7868e6338377c6b219fc6b1b53c7cba6f025
karen smith <karenkimberlysmith@gmail.com>
2019-03-15 14:08:26 -0700

    sign up form

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
    path('', views.home, name='home'),
    path('about/', views.about, name='about'),
+   path('feeds/', views.feeds, name='feeds'),
    path('accounts/', include('django.contrib.auth.urls')),
+   path('accounts/signup', views.signup, name='signup'),
 ]
