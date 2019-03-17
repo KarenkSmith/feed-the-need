77814d081ed46ec3d58fb471ebdcadd2217e9b54
Johnny Nguyen <j-h-n@outlook.com>
2019-03-16 20:23:53 -0700

    Add needed items to index.html

--- main/views.py
+++ main/views.py
@@ -2,32 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from .models import NeededItem
 
 # Views
 
 # Home
 def home(request):
-  return render(request, 'index.html')
+	needed_items = NeededItem.objects.all()
+	return render(request, 'index.html', {'ni': needed_items})
 
 # About
 def about(request):
-  return render(request, 'about.html')
+	return render(request, 'about.html')
 
 def feeds(request):
-  return render(request, 'feeds.html')
+	return render(request, 'feeds.html')
 
 def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      user = form.save()
-      # This is how we log a user in via code
-      login(request, user)
-      return redirect('feeds')
-    else:
-      error_message = 'Invalid credentials - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
+	error_message = ''
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+	if form.is_valid():
+		user = form.save()
+		# This is how we log a user in via code
+		login(request, user)
+		return redirect('feeds')
+	else:
+		error_message = 'Invalid credentials - try again'
+		# A bad POST or a GET request, so render signup.html with an empty form
+		form = UserCreationForm()
+		context = {'form': form, 'error_message': error_message}
+	return render(request, 'registration/signup.html', context)
