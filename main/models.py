b870e18ae38b35eac7e887cbf976d86e5698711d
Johnny Nguyen <j-h-n@outlook.com>
2019-03-16 17:56:03 -0700

    Add NeededItem model

--- main/models.py
+++ main/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Item(models.Model):
-	what = models.CharField(max_length=100)
+	what = models.CharField("select an item", max_length=100)
 	description = models.TextField(max_length=250)
 
 	def __str__(self):
 		return self.what
 
+class NeededItem(models.Model):
+	item = models.ForeignKey(Item, null=True, on_delete=models.SET_NULL)
+	description = models.TextField(max_length=250)
+	quantity = models.IntegerField()
+	location = models.TextField(max_length=250)
 
+	def __str__(self):
+		return self.item
