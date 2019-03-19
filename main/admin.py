b602fea9bb7402376b020fc83fcbab4484159d35
Felicity Nie <felicitysmusings@gmail.com>
2019-03-19 12:55:15 -0700

    fixed photos

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Item, NeededItem, Profile
+from .models import Item, NeededItem, Profile, Photo
 
 # Register models here
 admin.site.register(Item)
 admin.site.register(NeededItem)
 admin.site.register(Profile)
+admin.site.register(Photo)
